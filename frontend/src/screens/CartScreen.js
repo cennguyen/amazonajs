0fda8e752630301142cc6f826adcc40145235b3e
cennguyen <annp62923@gmail.com>
2020-12-29 16:51:14 +0700

    Implement addtocart, Make notification

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { addToCart } from '../actions/cartAction';
+import { useDispatch } from 'react-redux';
 
 export default function CartScreen(props) {
     const productID = props.match.params.id;
     const qty = props.location.search
         ? Number(props.location.search.split('=')[1])
         : 1;
+    const dispatch = useDispatch();
+    useEffect(() => {
+        if (productID) {
+            dispatch(addToCart(productID, qty));
+        }
+    }, [dispatch, productID, qty]);
     return (
         <div>
             <h1>Cart Screen</h1>
