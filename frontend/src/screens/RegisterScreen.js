2b1a10e9f35ac2a3656a048526ad666591a39d1e
cennguyen <annp62923@gmail.com>
2020-12-30 14:27:51 +0700

    update Register

--- frontend/src/screens/RegisterScreen.js
+++ frontend/src/screens/RegisterScreen.js
@@ -1,27 +1,33 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
-import { signin } from '../actions/userActions';
+import { register } from '../actions/userActions';
 import LoadingBox from '../components/LoadingBox';
 import MessageBox from '../components/MessageBox';
 
-export default function SigninScreen(props) {
+export default function RegisterScreen(props) {
 
+    const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
 
     const redirect = props.location.search
         ? props.location.search.split('=')[1]
         : '/';
 
-    const userSignin = useSelector((state) => state.userSignin);
-    const { userInfo, loading, error } = userSignin;
+    const userRegister = useSelector((state) => state.userRegister);
+    const { userInfo, loading, error } = userRegister;
 
     const dispatch = useDispatch();
     const submitHandler = (e) => {
         e.preventDefault();
-        //TODO: sigin action
-        dispatch(signin(email, password));
+        if (password !== confirmPassword) {
+            alert('Password and must match with confirm')
+        } else {
+            dispatch(register(name, email, password));
+        }
+
     };
     useEffect(() => {
         if (userInfo) {
@@ -34,9 +40,18 @@ export default function SigninScreen(props) {
                 <img className="signin" src="./images/sc1.jpg" alt="" />
             </div>
             <form className="form" onSubmit={submitHandler}>
-                <h1>Sign In</h1>
+                <h1>Create Account</h1>
                 {loading && <LoadingBox></LoadingBox>}
                 {error && <MessageBox variant="danger">{error}</MessageBox>}
+                <div>
+                    <label htmlFor="name">Name</label>
+                    <input
+                        type="text"
+                        id="name"
+                        placeholder="Enter name"
+                        required
+                        onChange={e => setName(e.target.value)}></input>
+                </div>
                 <div>
                     <label htmlFor="email">Email Address</label>
                     <input
@@ -46,6 +61,7 @@ export default function SigninScreen(props) {
                         required
                         onChange={e => setEmail(e.target.value)}></input>
                 </div>
+
                 <div>
                     <label htmlFor="password">Password</label>
                     <input
@@ -56,15 +72,27 @@ export default function SigninScreen(props) {
                         onChange={e => setPassword(e.target.value)}
                     ></input>
                 </div>
+
+                <div>
+                    <label htmlFor="confirmPassword">Confirm Password</label>
+                    <input
+                        type="password"
+                        id="confirmPassword"
+                        placeholder="Enter Confirm Password"
+                        required
+                        onChange={e => setConfirmPassword(e.target.value)}
+                    ></input>
+                </div>
+
                 <div>
                     <label />
-                    <button className="primary" type="submit">Sign In</button>
+                    <button className="primary" type="submit">Register</button>
                 </div>
                 <div>
                     <label />
                     <div>
-                        New Customer? {' '}
-                        <Link to={`/register?redirect=${redirect}`}>Create Account</Link>
+                        Already have an account? {' '}
+                        <Link to={`/signin?redirect=${redirect}`}>Sign In</Link>
                     </div>
                 </div>
             </form>
