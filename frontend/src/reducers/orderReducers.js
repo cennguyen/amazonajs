7b449acf0f386006ff178839c25bff0215de2fcf
cennguyen <annp62923@gmail.com>
2021-01-01 11:52:15 +0700

    Update order function and screen using paypal button

--- frontend/src/reducers/orderReducers.js
+++ frontend/src/reducers/orderReducers.js
@@ -25,7 +25,7 @@ export const orderCreateReducer = (state = {}, action) => {
     }
 };
 
-export const orderDetailsReducer = (state = { loading: true, order: {} }, action) => {
+export const orderDetailsReducer = (state = { loading: true }, action) => {
     switch (action.type) {
         case ORDER_DETAIL_REQUEST:
             return { loading: true };
