05676689f1425c18ec02395f782fc283a488b894
cennguyen <annp62923@gmail.com>
2021-01-02 14:17:34 +0700

    Update Profile

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -9,7 +9,10 @@ import {
     USER_SIGNIN_FAIL,
     USER_SIGNIN_REQUEST,
     USER_SIGNIN_SUCCESS,
-    USER_SIGNOUT
+    USER_SIGNOUT,
+    USER_UPDATE_PROFILE_FAIL,
+    USER_UPDATE_PROFILE_REQUEST,
+    USER_UPDATE_PROFILE_SUCCESS
 } from "../constants/userConstants"
 
 export const signin = (email, password) => async (dispatch) => {
@@ -71,5 +74,23 @@ export const detailsUser = (userID) => async (dispatch, getState) => {
             ? error.respone.data.message
             : error.message;
         dispatch({ type: USER_DETAILS_FAIL, payload: message })
+    }
+}
+
+export const updateUserProfile = (user) => async (dispatch, getState) => {
+    dispatch({ type: USER_UPDATE_PROFILE_REQUEST, payload: user });
+    const { userSignin: { userInfo } } = getState();
+    try {
+        const { data } = await Axios.put(`/api/users/profile`, user, {
+            headers: { Authorization: `Bearer ${userInfo.token}` },
+        });
+        dispatch({ type: USER_UPDATE_PROFILE_SUCCESS, payload: data });
+        dispatch({ type: USER_SIGNIN_SUCCESS, payload: data });
+        localStorage.setItem('userInfo', JSON.stringify(data));
+    } catch (error) {
+        const message = error.respone && error.respone.data.message
+            ? error.respone.data.message
+            : error.message;
+        dispatch({ type: USER_UPDATE_PROFILE_FAIL, payload: message })
     }
 }
