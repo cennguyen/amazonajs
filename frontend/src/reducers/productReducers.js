83a8eb667c11b8907eb6abfe838b5df37c5e09e0
cennguyen <annp62923@gmail.com>
2021-01-03 14:55:44 +0700

    Update Product

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -58,15 +58,15 @@ export const productCreateReducer = (state = {}, action) => {
 
 export const productUpdateReducer = (state = {}, action) => {
     switch (action.type) {
-        case PRODUCT_UPDATE_REQUEST:
-            return { loading: true };
-        case PRODUCT_UPDATE_SUCCESS:
-            return { loading: false, success: true };
-        case PRODUCT_UPDATE_FAIL:
-            return { loading: false, error: action.payload };
-        case PRODUCT_UPDATE_RESET:
-            return {};
-        default:
-            return state;
+      case PRODUCT_UPDATE_REQUEST:
+        return { loading: true };
+      case PRODUCT_UPDATE_SUCCESS:
+        return { loading: false, success: true };
+      case PRODUCT_UPDATE_FAIL:
+        return { loading: false, error: action.payload };
+      case PRODUCT_UPDATE_RESET:
+        return {};
+      default:
+        return state;
     }
-};
+  };
